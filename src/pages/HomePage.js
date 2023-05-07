123ce7fae3a343f4df63f756f19094e7fba1604e
VCassina <victorcassina@gmail.com>
2023-05-07 13:54:06 +0200

    Prettier improvement.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -17,17 +17,29 @@ function HomePage() {
       </nav>
       <h1>Bienvenue sur notre site</h1>
       {/* Content. */}
-      <div style={{
-  display: "grid",
-  gridTemplateColumns: "repeat(2, 1fr)",
-  gridTemplateRows: "repeat(2, 1fr)",
-  gap: "10px",
-}}>
-<div><Link to="/annonce">Annonce 1</Link></div><div><Link to="/annonce">Annonce 2</Link></div>
-<div><Link to="/annonce">Annonce 3</Link></div><div><Link to="/annonce">Annonce 4</Link></div>
-</div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(2, 1fr)",
+          gridTemplateRows: "repeat(2, 1fr)",
+          gap: "10px",
+        }}
+      >
+        <div>
+          <Link to="/annonce">Annonce 1</Link>
+        </div>
+        <div>
+          <Link to="/annonce">Annonce 2</Link>
+        </div>
+        <div>
+          <Link to="/annonce">Annonce 3</Link>
+        </div>
+        <div>
+          <Link to="/annonce">Annonce 4</Link>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default HomePage;
