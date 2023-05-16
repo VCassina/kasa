8b924c9c966fa422e3c577f981c182bfbc4710da
VCassina <victorcassina@gmail.com>
2023-05-17 00:23:42 +0200

    Fix gestion des erreurs, nouvelle façon de faire, abandon du ErrorDbArticle.js en l'état.

--- src/components/CollapseArticle.js
+++ src/components/CollapseArticle.js
@@ -11,39 +11,40 @@ function CollapseArticle(props) {
     setShowText(!showText);
   };
 
-  return (
-    <article className="collapseArticle_element-wrapper">
-      <div className="collapseArticle_element-title">
-        <h2>{props.title}</h2>
-        <FontAwesomeIcon
-          icon={faChevronDown}
-          className={`arrow-icon downCase ${showText ? "hidden" : ""}`} // Selon l'état de showText (qui s'inverse lors du click), le statut change, la classe est présente ou non.
-          onClick={toggleText}
-        />
-        <FontAwesomeIcon
-          icon={faChevronUp}
-          className={`arrow-icon upCase ${!showText ? "hidden" : ""}`}
-          onClick={toggleText}
-        />
-      </div>
-      <div className={`collapseArticle_element-texte ${showText ? "" : "hidden"}`}>
-      {Array.isArray(props.content) && props.content.length > 0 ? (
-        <div>
-          {props.content.map((item, index) => (
-            <span key={index}>
-              {item}
-              <br />
-            </span>
-          ))}
-        </div>
-      ) : props.content ? (
-        <p>{props.content}</p>
-      ) : (
-        <p>- Texte manquant -</p>
-      )}
+return (
+  <article className="collapseArticle_element-wrapper">
+    <div className="collapseArticle_element-title">
+      <h2>{props.title}</h2>
+      <FontAwesomeIcon
+        icon={faChevronDown}
+        className={`arrow-icon downCase ${showText ? "hidden" : ""}`}
+        onClick={toggleText}
+      />
+      <FontAwesomeIcon
+        icon={faChevronUp}
+        className={`arrow-icon upCase ${!showText ? "hidden" : ""}`}
+        onClick={toggleText}
+      />
     </div>
-    </article>
-  );
+    <div className={`collapseArticle_element-texte ${showText ? "" : "hidden"}`}>
+    {Array.isArray(props.content) && props.content.length > 0 ? ( // Si c'est un tableau non vide :
+    <div>
+      {props.content.map((item, index) => (
+        <span key={index}>
+          {item}
+          <br />
+        </span>
+      ))}
+    </div>
+  ) : props.content && !Array.isArray(props.content) ? (  // Si props.content est trouvé et qu'il ne s'agit pas d'un tableau.
+    <p>{props.content}</p>
+  ) : ( // Sinon (si props.content est trouvé mais est vide ou s'il s'agit d'un tableau(props.content) vide)...
+    <p>- Texte manquant -</p> // 
+  )}
+</div>
+  </article>
+);
+
 }
 
 export default CollapseArticle;
